8efb8725111662aef04d4c199e718abdfaaa02c4
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-08-26 08:49:02 +0530

    bug fix in stripe controller

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -49,7 +49,7 @@ class StripeController extends Controller
         $data['customer'] = $customer['id'];
 
         try {
-            $client_details = Client::where('email', $requestData['emailAddress'])->first();
+            $client_details = Client::where('email', $requestData['email'])->first();
             $order_table = new OrderDetails();
 
             $order_table->session_id = $customer['id'];
@@ -69,7 +69,7 @@ class StripeController extends Controller
             $order_table->baby_seats = "N/A";
             $order_table->booster_seats = "N/A";
             $order_table->wheel_chairs = "N/A";
-            $order_table->total_price = "N/A";
+            $order_table->total_price = $requestData['amount'];
             $order_table->payment_status = 'unpaid';
             $order_table->order_status = 'pending';
 
@@ -83,32 +83,20 @@ class StripeController extends Controller
 
     public function addPaymentSuccessLog(Request $request) {
 
-        $payment_details = OrderDetails::where(['session_id' => $request->session_id, 'payment_status' => 'unpaid'])->first();
+        try {
+            $updatePaymentLog = OrderDetails::where([
+                'session_id' => $request->session_id, 'payment_status' => 'unpaid'
+            ])->update([
+                'payment_status' => 'paid'
+            ]);
 
-        if ($payment_details != null) {
-            $payment_details->vehicle_id = $request->vehicle_id;
-            $payment_details->from = $request->from;
-            $payment_details->to = $request->to;
-            $payment_details->trip_type = $request->trip_type;
-            $payment_details->passengers = $request->passengers;
-            $payment_details->luggage = $request->luggage;
-            $payment_details->date_and_time = $request->date_and_time;
-            $payment_details->return_date_and_time = $request->return_date_and_time;
-            $payment_details->flight_number = $request->flight_number;
-            $payment_details->return_from = $request->return_from;
-            $payment_details->return_to = $request->return_to;
-            $payment_details->meet = "N/A";
-            $payment_details->baby_seats = $request->baby_seats;
-            $payment_details->booster_seats = "N/A";
-            $payment_details->wheel_chairs = "N/A";
-            $payment_details->total_price = $request->amount;
-            $payment_details->payment_details = "paid";
-            $payment_details->order_status = 'pending';
-            $payment_details->save();
-
-            return (new AppHelper())->responseMessageHandle(200, "Operation Complete.");
-        } else {
-            return (new AppHelper())->responseMessageHandle(404, "Not Matched Session.");
+            if ($updatePaymentLog) {
+                return (new AppHelper())->responseMessageHandle(200, "Operation Complete.");
+            } else {
+                return (new AppHelper())->responseMessageHandle(404, "Not Matched Session.");
+            }
+        } catch (\Exception $e) {
+            return (new AppHelper())->responseMessageHandle(500, $e->getMessage());
         }
     }
 }
